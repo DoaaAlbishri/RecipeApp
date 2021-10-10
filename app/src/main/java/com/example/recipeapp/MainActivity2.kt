b49d6f55d995c7b98dd8f6d87931f9f9b17a7865
Doaa <Duaa_ali@outlook.sa>
2021-10-10 16:57:56 +0300

    Second commit with RV

--- app/src/main/java/com/example/recipeapp/MainActivity2.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity2.kt
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity2 : AppCompatActivity() {
+    val words = arrayListOf<String>()
+    lateinit var myRv : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val textView = findViewById<TextView>(R.id.textView)
+        //val textView = findViewById<TextView>(R.id.textView)
         val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
+        myRv = findViewById(R.id.recyclerView)
 
         //progress
         val progressDialog = ProgressDialog(this)
@@ -36,12 +41,18 @@ class MainActivity2 : AppCompatActivity() {
                 for(Recipe in resource!!){
                     recipeData = recipeData +Recipe.title+ "\n"+Recipe.author + "\n"+Recipe.ingredients + "\n"+Recipe.instructions +"\n"+"\n"
                 }
-                textView.text= recipeData
+             //textView.text= recipeData
+                words.add(recipeData.toString())
+                rv()
             }
             override fun onFailure(call: Call<List<RecipeDetails.Datum>>, t: Throwable) {
                 progressDialog.dismiss()
                 Toast.makeText(applicationContext, ""+t.message, Toast.LENGTH_SHORT).show();
             }
         })
+    }
+    fun rv(){
+        myRv.adapter = RecyclerViewAdapter(words)
+        myRv.layoutManager = LinearLayoutManager(this)
     }
 }
